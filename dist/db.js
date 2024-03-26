514c25f5860559990bbcc3d53236149ef4bdefe3
aaditya shewale <aadityashewale@aadityas-MacBook-Air.local>
2024-03-26 09:46:19 +0530

    db

---
+++ dist/db.js
@@ -0,0 +1,36 @@
+"use strict";
+// Importing necessary modules
+const mysql = require('mysql');
+require('dotenv').config();
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: 20000, // Increase connection timeout (milliseconds)
+    timeout: 20000
+});
+// Execute the SQL query to create the table
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    // Use the connection to execute the SQL query
+    connection.query(`
+      CREATE TABLE IF NOT EXISTS your_table_name (
+        name VARCHAR(255),
+        uploading BOOLEAN,
+        deployed BOOLEAN
+      )
+    `, (error, results) => {
+        // When done with the connection, release it
+        connection.release();
+        if (error) {
+            console.error('Error creating table:', error);
+            return;
+        }
+        console.log('Table created successfully:', results);
+    });
+});
